d0c1b0a35dc2876f478a504a2b6bd691c041d266
Luke Walsh <lhwalsh@cpp.edu>
2016-12-15 22:19:19 -0800

    working on creating a user input

--- Maze.java
+++ Maze.java
@@ -1,5 +1,24 @@
+import java.util.Scanner
 public class Maze {
+
+    private int length;
+
     public Maze() {
+	Scanner user = new Scanner(System.in);
+	length = user.nextInt();
+	for (int i = 0; i < length*2+1; i++) {
+	    System.out.println("*");
+	}
+	for (int i = 0; i < length; i++) {
+	    for (int j = 0; j < length*2+1; i++) {
+		if (i%2 == 0) {
+		    System.out.println("*");
+		}
+	    }
+	}
+	for (int i = 0; i < length*2+1; i++) {
+	    System.out.println("*");
+	}
 
     }
 }
