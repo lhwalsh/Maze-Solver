4f8d8903b50a24bb231369e792a6a3240881470c
Luke Walsh <lhwalsh@cpp.edu>
2016-12-15 22:27:23 -0800

    more updates on maze tostring

--- Maze.java
+++ Maze.java
@@ -1,24 +1,34 @@
-import java.util.Scanner
+import java.util.Scanner;
 public class Maze {
 
     private int length;
 
     public Maze() {
 	Scanner user = new Scanner(System.in);
+	System.out.println("Please enter the length of the puzzle: ");
 	length = user.nextInt();
+	System.out.println(length);
+	
+    }
+
+    public String toString() {
 	for (int i = 0; i < length*2+1; i++) {
-	    System.out.println("*");
+	    System.out.print("*");
 	}
+	System.out.println();
 	for (int i = 0; i < length; i++) {
 	    for (int j = 0; j < length*2+1; i++) {
 		if (i%2 == 0) {
-		    System.out.println("*");
+		    System.out.print("*");
+		} else {
+		    System.out.print(" ");
 		}
 	    }
 	}
+	System.out.println();
 	for (int i = 0; i < length*2+1; i++) {
-	    System.out.println("*");
+	    System.out.print("*");
 	}
-
     }
+
 }
